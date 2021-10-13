57640eb7ef5591a3efe92d9929f6e8eb4a2536be
Curtis Nash <curtis.r.nash@gmail.com>
2021-10-13 12:33:59 -0600

    first commit on main

---
+++ 05-jumper/wordBank.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace _05_jumper
+{
+    class wordBank
+    {
+        public list<str> _words;
+        public String _seceretWord;
+
+        public void SelectRandomWord()
+        {
+            _words.add("tacos");
+        }
+
+        public void CheckInWord()
+        //checks if the guess from user service is in the secret word
+        {
+            Console.WriteLine("Starting point for the Jumper project.");
+        }
+
+        public void DisplayWord()
+        {
+            Console.WriteLine("Starting point for the Jumper project.");
+        }
+    }
+}
