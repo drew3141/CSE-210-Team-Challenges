4b7507aff8f05830fbb307ae6f721a50a0691b61
Drew Howells <drew3141@gmail.com>
2021-10-20 12:21:10 -0600

    Shell of Director/Program

--- 06-mastermind/Director.cs
+++ 06-mastermind/Director.cs
@@ -4,9 +4,32 @@ namespace _06_mastermind
 {
     class Director
     {
-        public void stuff()
+        private bool _keepPlaying = true;
+        public void StartGame()
         {
-            
+            Board b = new Board();
+            Player player1 = new Player();
+            Player player2 = new Player();
+            Mastermind codeMaster = new Mastermind();
+            while (_keepPlaying)
+            {
+                GetInputs();
+                DoUpdates();
+                DoOutputs();
+            }
+        }
+
+        public void GetInputs()
+        {
+
+        }
+        public void DoUpdates()
+        {
+
+        }
+        public void DoOutputs()
+        {
+
         }
     }
 }
