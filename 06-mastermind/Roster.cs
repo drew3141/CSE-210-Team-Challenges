511d8cbbebcea15f09ca35db708b2715d2b0d575
Curtis Nash <curtis.r.nash@gmail.com>
2021-10-20 12:22:29 -0600

    All classes

---
+++ 06-mastermind/Roster.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace _06_mastermind
+{
+    class Roster
+    {
+        private int _currentPlayerIndex = 0;
+        private List<Player> _players = new List<Player>();
+
+        public void AddPlayer(Player player)
+        {
+            _players.Add(player);
+        }
+
+        public Player GetCurrentPlayer()
+        {
+            return _players[_currentPlayerIndex];
+        }
+
+        public void AdvanceNextPlayer()
+        {
+            _currentPlayerIndex = (_currentPlayerIndex + 1) % _players.Count;
+        }
+    }
+}
---
+++ 06-mastermind/UserService.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace _06_mastermind
+{
+    public class UserService
+    {
+        public void DisplayText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public int GetNumberInput(string promptText)
+        {
+            Console.Write(promptText);
+            string userInput = Console.ReadLine();
+
+            int numericChoice = int.Parse(userInput);
+            return numericChoice;
+        }
+
+        public string GetStringInput(string promptText)
+        {
+            Console.Write(promptText);
+            string userInput = Console.ReadLine();
+
+            return userInput;
+        }
+    }
+}
