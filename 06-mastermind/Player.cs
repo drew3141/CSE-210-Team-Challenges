b90b6b075cf8c2e19b2d7e687c4244a95d3401c9
Curtis Nash <curtis.r.nash@gmail.com>
2021-10-20 12:21:08 -0600

    first commit

--- 06-mastermind/Player.cs
+++ 06-mastermind/Player.cs
@@ -4,9 +4,27 @@ namespace _06_mastermind
 {
     class Player
     {
-        public void stuff()
+        private String _name;
+        private Mastermind _move;
+
+        public Player(String name)
         {
-            
+            _name = name;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetMove(Mastermind move)
+        {
+            _move = move;
+        }
+
+        public Mastermind GetMove()
+        {
+            return _move;
         }
     }
 }
