abecddb363b9f159fe5a80751db4175ac6d50f83
E <elijahw@byui.edu>
2021-10-21 18:05:38 -0600

    eli

--- 06-mastermind/Board.cs
+++ 06-mastermind/Board.cs
@@ -4,9 +4,20 @@ namespace _06_mastermind
 {
     class Board
     {
-        public void stuff()
+        private string lines = "---------------------";
+        private string player = "Player ";
+        
+        public void DisplayBoard(Player one, Player two)
         {
-            
+            DisplayLines();
+            Console.WriteLine(player + one.GetName() + ": ");
+            Console.WriteLine(player + two.GetName() + ": ");
+            DisplayLines();
         }
+        public void DisplayLines()
+        {
+            Console.WriteLine(lines);
+        }
+    
     }
 }
