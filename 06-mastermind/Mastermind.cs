9cbb13dc8116843eb87ddd5cdc5dc4a8b38516a0
Kennion Gubler <gublerkennion@gmail.com>
2021-10-21 19:00:48 -0600

    basic logic

--- 06-mastermind/Mastermind.cs
+++ 06-mastermind/Mastermind.cs
@@ -4,9 +4,44 @@ namespace _06_mastermind
 {
     class Mastermind
     {
-        public void stuff()
+        string guess = "1435";
+        public string createCode()
         {
-            
+            Random r = new Random();
+            int number = r.Next(1000, 9999);
+            string code = number.ToString();
+            return code;
+        }
+        public string[] getHint(string code, string guess)
+        {
+            var hint = new string[] {"P","P","P","P" };
+            string[] guess_list = guess.Split();
+            string[] code_list = code.Split();
+            if(guess_list[0] == code_list[0]){
+                hint[0] = "X";
+            }
+            else if(guess_list[1] == code_list[1]){
+                hint[1] = "X";
+            }
+            else if(guess_list[2] == code_list[2]){
+                hint[2] = "X";
+            }
+            else if(guess_list[3] == code_list[3]){
+                hint[3] = "X";
+            }
+            return hint;
+
+        }
+        public bool hasWon(string code, string guess)
+        {
+            bool hasWon = false;
+            if(code == guess){
+                hasWon = true;
+            }
+            else{
+                hasWon = false;
+            }
+            return hasWon;
         }
     }
 }
