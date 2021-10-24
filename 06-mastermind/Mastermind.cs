befff0d50b1c8f872b79b6ecae533997340cbb31
Kennion Gubler <gublerkennion@gmail.com>
2021-10-23 22:17:26 -0600

    Xs show up

--- 06-mastermind/Mastermind.cs
+++ 06-mastermind/Mastermind.cs
@@ -10,6 +10,8 @@ namespace _06_mastermind
         private string hint = "----";
         private string lastHint = "----";
 
+        char[] hintList = {'*', '*', '*', '*'};
+
         public void setGuess(string userInput){
             previousGuess= guess;
             guess = userInput;
@@ -26,8 +28,29 @@ namespace _06_mastermind
         }
         public void setHint()
         {
-            lastHint = hint;
-            hint = "****";
+            char[] codeArray = code.ToCharArray();
+            char[] guessArray = guess.ToCharArray();
+            if(guessArray[0] == codeArray[0])
+            {
+                hintList[0] = 'X';
+            }
+            else if(guessArray[1] == codeArray[1])
+            {
+                hintList[1] = 'X';
+            }
+            else if(guessArray[2] == codeArray[2])
+            {
+                hintList[2] = 'X';
+            }
+            else if(guessArray[3] == codeArray[3])
+            {
+                hintList[3] = 'X';
+            }
+            // else if(guessArray.contains())
+            // else{
+            //     hintList = hintList;
+            // }
+            hint = new string(hintList);
         }
         public string getHint()
         {
