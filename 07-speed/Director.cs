e8fe709667460d4a62671d720ec96fc85ddbe622
Drew Howells <drew3141@gmail.com>
2021-11-01 11:53:25 -0600

    Basic director shell

--- 07-speed/Director.cs
+++ 07-speed/Director.cs
@@ -4,9 +4,36 @@ namespace _07_speed
 {
     class Director
     {
+        private bool _keepPlaying = true;
         public void StartGame()
         {
-            //Do Stuff;
+            Setup();
+            while (_keepPlaying)
+            {
+                DoOutputs();
+                GetInputs();
+                DoUpdates();
+            }
+        }
+
+        public void Setup()
+        {
+
+        }
+
+        public void GetInputs()
+        {
+
+        }
+
+        public void DoUpdates()
+        {
+
+        }
+
+        public void DoOutputs()
+        {
+
         }
     }
 }
