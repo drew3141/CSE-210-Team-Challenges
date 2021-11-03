b285a359cbd7caf1868d3a8fe4df18923afc0971
Curtis Nash <curtis.r.nash@gmail.com>
2021-11-03 12:32:28 -0600

    Last (hopefully) word commit

--- 07-speed/Word.cs
+++ 07-speed/Word.cs
@@ -7,15 +7,30 @@ namespace _07_speed
 {
     public class Word
     {
-        protected void AddWordList()
+        protected List<string> _words = new List<string>();
+        protected string _word = "";
+        protected int _wordIndex = -1;
+
+        protected Word()
         {
-            List <string> wordList = new List<string>();
-            wordList.Add("Hello");
-            wordList.Add("Goodbye");
-            wordList.Add("");
+            ReadWordList();
         }
-        
+
+        protected void ReadWordList()
+        {
+            String[] fileWords = System.IO.File.ReadAllLines(@"Words.txt");
+            _words.AddRange(fileWords);
+        }
+
+        protected string SelectRandomWord()
+        {
+            Random r = new Random();
+            _wordIndex = r.Next(0, _words.Count);
+            _word = _words[_wordIndex];
+            Console.WriteLine(_word);
+
+            return _word;
+        }
+         
     }
 }
-
-
